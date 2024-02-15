0bc382f97bca42cfdd9614183e33d531a6cf8298
Andre Hansen <Andre.Hansen@aenergi.no>
2024-02-15 08:12:11 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import xml.etree.ElementTree as ET
-from flask import Flask, jsonify
+from flask import Flask
 
 tree = ET.parse("seeds.xml")
 root = tree.getroot()
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello',  methods=['GET'])
 def home():
     return "hello"
 
-@app.route('/add', methods=['PUT'])
-def add():
-    new_seed = ET.SubElement(root, "Seed")
-    new_seed.set("genes", "XXXXXX")
-    tree.write("seeds.xml")
+# @app.route('/add',  methods=['PUT'])
+# def add():
+#     new_seed = ET.SubElement(root, "Seed")
+#     new_seed.set("genes", "XXXXXX")
+#     tree.write("seeds.xml")
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
